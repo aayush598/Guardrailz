886c574a10998253cf2c2c6f3d30d3845caca331
aayush598 <aayushgid598@gmail.com>
2026-01-14 01:48:42 +0530

    style: format the complet code base

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals','plugin:@typescript-eslint/recommended'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['.next/**', 'node_modules/**', 'dist/**', 'coverage/**'],
   rules: {
     '@typescript-eslint/no-unused-vars': ['error'],
